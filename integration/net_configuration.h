474dca6bb3449a24401028315e6a8f7f9a88da54
virtan <virtan@virtan.com>
2016-08-16 14:09:23 +0700

    std::isnan

--- integration/net_configuration.h
+++ integration/net_configuration.h
@@ -51,7 +51,7 @@ int restore_int(float a) {
 bool compare(vec_t a, vec_t b) {
     if(a.size() != b.size()) return false;
     for(size_t i = 0; i < a.size(); ++i)
-        if(isnan(a[i]) || isnan(b[i]) || fabs(a[i] - b[i]) >= 0.01) return false;
+        if(std::isnan(a[i]) || std::isnan(b[i]) || fabs(a[i] - b[i]) >= 0.01) return false;
     return true;
 }
 
