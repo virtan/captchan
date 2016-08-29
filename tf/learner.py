73fc62c5dab160bc8c1fa8112949314da3e7ae43
Igor Milyakov <imilyakov@mz.com>
2016-08-29 07:14:06 +0000

    fixed the model

--- tf/learner.py
+++ tf/learner.py
@@ -40,33 +40,62 @@ h_pool2 = max_pool_2x2(h_conv2) # 110x30x32 -> 55x15x32
 
 h_pool2_flat = tf.reshape(h_pool2, [-1, 55*15*32])
 
+drop = tf.nn.dropout(h_pool2_flat, keep_prob)
+
+W_id = weight_variable([55 * 15 * 32, 6 * 256])
+b_id = bias_variable([6 * 256])
+pre_net = tf.nn.relu(tf.matmul(drop, W_id) + b_id)
+net = tf.reshape(pre_net, [-1, 6*256])
+
 branches = []
-for i in range(0, 6):
-    W_fc1 = weight_variable([55 * 15 * 32, 256])
+for i in range(6):
+    b_drop = tf.nn.dropout(net, keep_prob)
+    W_fc1 = weight_variable([6 * 256, 256])
     b_fc1 = bias_variable([256])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-    W_fc2 = weight_variable([256, 1])
-    b_fc2 = bias_variable([1])
-    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-    branches.append(y_conv)
+    h_fc1 = tf.nn.relu(tf.matmul(b_drop, W_fc1) + b_fc1)
+    h_fc1r = tf.reshape(h_fc1, [-1, 256])
+    #W_fc2 = weight_variable([1024, 1])
+    #b_fc2 = bias_variable([1])
+    #y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    branches.append(h_fc1r)
 
 #y_ = tf.pack(branches, axis=0)
-y_ = tf.concat(1, branches)
-pprint.pprint(y_)
+concatenated = tf.concat(0, branches)
+conc_flat = tf.reshape(concatenated, [-1, 6*256])
+conc_drop = tf.nn.dropout(conc_flat, keep_prob)
+W_final = weight_variable([6*256, 6])
+b_final = bias_variable([6])
+y = tf.nn.softmax(tf.matmul(conc_drop, W_final) + b_final)
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.round(y_conv), tf.round(y_))
+pprint.pprint(y)
+
+#cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+reduced_difference = tf.reduce_mean(tf.sub(y_, y))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(reduced_difference)
+correct_prediction = tf.equal(tf.round(y_), tf.round(y))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+print "Loading values..."
 offset = 0
 raw_values = np.loadtxt("../abc_values.gz")
 amount = raw_values.size / 6
+print amount, "loaded"
+print "Reshaping..."
 values = np.reshape(raw_values, (amount, 6))
 gc.collect()
+print "Loading data..."
 data = np.reshape(np.loadtxt("../abc_data.gz"), (amount, 220*60))
+print "Loaded"
+
 gc.collect()
+print "Garbage collected"
+
+print "Medium pic:"
+print data[amount/2,:]
+print "Medium values:"
+print values[amount/2,:]
+
+print "Starting training"
 
 epochs_completed = 0
 
@@ -86,7 +115,7 @@ def next_batch(batch_size):
 sess = tf.InteractiveSession()
 sess.run(tf.initialize_all_variables())
 
-for i in range(20000):
+for i in range(500*amount/50):
     batch = next_batch(50)
     if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
