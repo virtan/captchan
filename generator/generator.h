b37492192d6e41961b2b5b54ba4f632807424aec
virtan <virtan@virtan.com>
2016-09-03 09:12:01 +0700

    simplifying

--- generator/generator.h
+++ generator/generator.h
@@ -7,6 +7,7 @@
 std::string generate_png(const std::string &text);
 std::string generate_png_light(const std::string &text);
 std::vector<float> generate_vec(const std::string &text);
+std::vector<float> generate_vec_light(const std::string &text);
 struct captcha {
     std::string png;
     std::vector<float> vec;
