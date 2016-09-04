f69c5209e391289ea8e40c967b1a4a7047b2b64b
Igor Milyakov <imilyakov@mz.com>
2016-09-04 17:22:19 +0000

    working with a single digit

--- tf/mnist.py
+++ tf/mnist.py
@@ -17,12 +17,14 @@ def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(x, W, P):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=P)
+    #return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-            strides=[1, 2, 2, 1], padding='SAME')
+            strides=[1, 2, 2, 1], padding='VALID')
+            #strides=[1, 2, 2, 1], padding='SAME')
 
 def show_img(data, name):
     w, h = data.shape
@@ -35,38 +37,42 @@ def show_img(data, name):
 sess = tf.InteractiveSession()
 
 x = tf.placeholder(tf.float32, shape=[None, 60*60])
-#x = tf.placeholder(tf.float32, shape=[None, 28*28])
+#x = tf.placeholder(tf.float32, shape=[None, 32*32])
 y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 W_conv1 = weight_variable([5, 5, 1, 6])
 b_conv1 = bias_variable([6])
 
+#x_image = tf.reshape(x, [-1,60,60,1])
 x_image = tf.reshape(x, [-1,60,60,1])
-#x_image = tf.reshape(x, [-1,28,28,1])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1, 'SAME') + b_conv1) # 60x60x1 -> 60x60x6
+h_pool1 = max_pool_2x2(h_conv1) # 60x60x6 -> 30x30x6
 
 W_conv2 = weight_variable([5, 5, 6, 16])
 b_conv2 = bias_variable([16])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 'VALID') + b_conv2) # 30x30x6 -> 26x26x16
+h_pool2 = max_pool_2x2(h_conv2) # 26x26x16 -> 13x13x16
 
-W_fc1 = weight_variable([15 * 15 * 64, 1024])
+W_conv3 = weight_variable([13, 13, 16, 120])
+b_conv3 = bias_variable([120])
+h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3, 'VALID') + b_conv3) # 13x13x16 -> 1x1200
+
+W_fc1 = weight_variable([1*120, 84])
 #W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
+b_fc1 = bias_variable([84])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 15*15*64])
+h_pool2_flat = tf.reshape(h_conv3, [-1, 1*120])
 #h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1) # 5x5x16 -> 1x120
 
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, 10])
+W_fc2 = weight_variable([84, 10])
 b_fc2 = bias_variable([10])
 
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2) # 1x84 -> 1x10
 
 print "Loading values..."
 offset = 0
@@ -118,8 +124,11 @@ epochs_completed = 0
 
 def next_batch(batch_size):
     global offset, data, values, epochs_completed
-    if offset + batch_size > amount:
+    if offset + batch_size >= amount:
         epochs_completed += 1
+        print("epoch %d completed"%epochs_completed)
+        if epochs_completed == 3:
+            return False
         perm = np.arange(amount)
         np.random.shuffle(perm)
         values = values[perm]
@@ -136,8 +145,10 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 sess.run(tf.initialize_all_variables())
-for i in range(20000):
+for i in range(2000000):
     batch = next_batch(50)
+    if batch == False:
+        break
     if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
             x:batch[0], y_: batch[1], keep_prob: 1.0})
