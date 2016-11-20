436bc7cd201eee062e8f7e8120d56f19dbac8da7
virtan <virtan@virtan.com>
2016-11-20 14:25:18 +0700

    seems to be working

--- tf/qrunner.py
+++ tf/qrunner.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
 import tensorflow as tf
-import os.path
 
 def read_pngs(filename_queue):
     image_reader = tf.WholeFileReader()
     image_filename, image_file = image_reader.read(filename_queue)
-    print(image_filename.eval())
+    #print(image_filename.eval())
     image = tf.image.decode_png(image_file, channels=1)
     image = tf.reshape(image, [220, 60, 1])
     label_reversed_str = tf.reverse(tf.decode_raw(image_filename, tf.uint8),
@@ -16,17 +15,15 @@ def read_pngs(filename_queue):
     #print(label_number.eval())
     label = tf.one_hot(label_number, 10, 1.0, 0.0, -1)
     label = tf.reshape(label, [60])
-    print(label.eval())
+    #print(label.eval())
     #print(image.eval())
     return image, label
 
 def input_pipeline(filenames, batch_size, read_threads, num_epochs = None):
     filename_queue = tf.train.string_input_producer(
             filenames, num_epochs = num_epochs, shuffle = True)
-    return read_pngs(filename_queue)
-    #image_list = [read_pngs(filename_queue)
-                  #for _ in range(read_threads)]
-    return 0
+    image_list = [read_pngs(filename_queue)
+                  for _ in range(read_threads)]
     #print(image_list)
     # min_after_dequeue defines how big a buffer we will randomly sample
     #   from -- bigger means better shuffling but slower start up and more
@@ -34,25 +31,25 @@ def input_pipeline(filenames, batch_size, read_threads, num_epochs = None):
     # capacity must be larger than min_after_dequeue and the amount larger
     #   determines the maximum we will prefetch.  Recommendation:
     #   min_after_dequeue + (num_threads + a small safety margin) * batch_size
-    # min_after_dequeue = 10
-    # capacity = min_after_dequeue + 3 * batch_size
-    # image_batch, label_batch = tf.train.shuffle_batch_join(
-    #   image_list, batch_size = batch_size, capacity = capacity,
-    #   min_after_dequeue = min_after_dequeue)
-    # return image_batch, label_batch
+    min_after_dequeue = 10
+    capacity = min_after_dequeue + 3 * batch_size
+    image_batch, label_batch = tf.train.shuffle_batch_join(
+      image_list, batch_size = batch_size, capacity = capacity,
+      min_after_dequeue = min_after_dequeue)
+    return image_batch, label_batch
 
 
-filenames = tf.train.match_filenames_once("../images/i_*.png")
 sess = tf.Session()
 
 with sess.as_default():
-    tf.initialize_all_variables().run()
     coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord = coord)
+    filenames = tf.train.match_filenames_once("../images/i_*.png")
     pipeline = input_pipeline(filenames, 50, 1, 1)
-    #print(pipeline[1].eval())
-    #image_tensor = sess.run(pipeline[0])
-    #print(image_tensor)
+    init_op = tf.group(tf.initialize_all_variables(), tf.initialize_local_variables())
+    sess.run(init_op)
+    threads = tf.train.start_queue_runners(coord = coord)
+    image_tensor = sess.run(pipeline[1])
+    print(image_tensor)
 
 ##     num_preprocess_threads = 1
 ##     min_queue_examples = 256
