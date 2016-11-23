73c10f6e7d5eb82d09b33c8104409cdf04a05d1f
Igor Milyakov <imilyakov@machinezone.com>
2016-11-22 17:33:33 -0800

    more optimizations

--- tf/qrunner.py
+++ tf/qrunner.py
@@ -19,11 +19,10 @@ with sess.as_default():
     y_lreshaped = tf.reshape(m.y, [-1, 10])
     y_expected_reshaped = tf.reshape(m.y_expected, [-1, 6, 10])
     y_expected_lreshaped = tf.reshape(m.y_expected, [-1, 10])
-    cross_entropy = tf.reduce_mean(tf.reshape(tf.nn.softmax_cross_entropy_with_logits(y_lreshaped, y_expected_lreshaped), [-1, 6]))
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_lreshaped, y_expected_lreshaped))
     tf.scalar_summary('cross_entropy', cross_entropy)
-    #ce_summ = tf.scalar_summary("cross entropy", cross_entropy)
-    # reduced_difference = tf.reduce_mean(tf.abs(tf.sub(y_, y_conv)))
-    train_op = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    #train_op = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+    train_op = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
     correct_prediction = tf.reduce_all(tf.equal(tf.argmax(y_reshaped, 1), tf.argmax(y_expected_reshaped,1)), [1])
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     tf.scalar_summary('accuracy', accuracy)
@@ -48,11 +47,32 @@ with sess.as_default():
             #    ce.show_img(np.reshape(batch_x[0], (220,60)), 'pic')
             #    print "pic generated"
             if i%100 == 0:
-                test_summary, test_accuracy = sess.run([merged, accuracy], feed_dict = {
+                y_reshaped_, y_lreshaped_, y_expected_reshaped_, y_expected_lreshaped_, cross_entropy_, correct_prediction_, accuracy_, test_summary, test_accuracy = sess.run([y_reshaped, y_lreshaped, y_expected_reshaped, y_expected_lreshaped, cross_entropy, correct_prediction, accuracy, merged, accuracy], feed_dict = {
                         m.x: batch_x, m.y_expected: batch_y_expected,
                         m.keep_prob: 1.0})
                 test_writer.add_summary(test_summary, i)
                 print("step %d, training accuracy %g"%(i, test_accuracy))
+                print "y_reshaped:\n"
+                print y_reshaped
+                print y_reshaped_
+                print "y_lreshaped:\n"
+                print y_lreshaped
+                print y_lreshaped_
+                print "y_expected_reshaped:\n"
+                print y_expected_reshaped
+                print y_expected_reshaped_
+                print "y_expected_lreshaped:\n"
+                print y_expected_lreshaped
+                print y_expected_lreshaped_
+                print "cross_entropy:\n"
+                print cross_entropy
+                print cross_entropy_
+                print "correct_prediction:\n"
+                print correct_prediction
+                print correct_prediction_
+                print "accuracy:\n"
+                print accuracy
+                print accuracy_
                 save_path = saver.save(sess, "model.ckpt")
             train_summary, _ = sess.run([merged, train_op], feed_dict = {
                 m.x: batch_x, m.y_expected: batch_y_expected, 
